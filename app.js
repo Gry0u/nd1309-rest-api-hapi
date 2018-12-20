8cae04090663bf96a9fa9f66457c54287f182959
Gry0u <gauthriou@protonmail.com>
2018-12-20 22:50:14 +0100

    Add GET and POST API Endpoints

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-const Hapi = require('hapi');
+const Hapi = require('hapi')
 
 /**
  * Class Definition for the REST API
  */
 class BlockAPI {
+/**
+* Constructor that allows initialize the class
+*/
+  constructor () {
+    this.server = Hapi.Server({
+      port: 3000,
+      host: 'localhost'
+    })
+    this.initControllers()
+    this.start()
+  }
 
-    /**
-     * Constructor that allows initialize the class 
-     */
-    constructor() {
-		this.server = Hapi.Server({
-            port: 3000,
-            host: 'localhost'
-        });
-        this.initControllers();
-        this.start();
-    }
-
-    /**
+  /*
      * Initilization of all the controllers
      */
-	initControllers() {
-		require("./BlockController.js")(this.server);
-	}
-    
-    async start() {
-        await this.server.start();
-        console.log(`Server running at: ${this.server.info.uri}`);
+  initControllers () {
+    require("./BlockController.js")(this.server)
+  }
+  async start () {
+    try {
+      await this.server.start()
+      console.log(`Server running at: ${this.server.info.uri}`)
+    } catch (err) {
+      console.log(err)
+      process.exit(1)
     }
-
+  }
 }
 
-new BlockAPI();
+new BlockAPI()
